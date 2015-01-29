9e121428b2765b184539ccfe4e33e027d2609c52
liuyang <liuyang@xiangrikui.com>
2015-01-29 14:52:32 +0800

    项目初始化

---
+++ src/main/java/com/lala/mongodb/App.java
@@ -0,0 +1,13 @@
+package com.lala.mongodb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
